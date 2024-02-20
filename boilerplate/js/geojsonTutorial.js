bd58c8486f1cf918f6a2c54b4f5de8c7a39177f8
Yanbing Chen <yanbing.chen@mail.mcgill.ca>
2024-02-20 16:50:22 -0600

    quickstartTutorial

---
+++ boilerplate/js/geojsonTutorial.js
@@ -0,0 +1,44 @@
+/* Example from Leaflet GeoJSON Tutorial */
+
+var map = L.map('map').setView([39.75, -104.99], 13);
+
+//add tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+// Create GeoJSON and add it to the map
+L.geoJSON(geojsonFeature).addTo(map);
+
+//  GeoJSON is passed as an array of valid GeoJSON objects
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
