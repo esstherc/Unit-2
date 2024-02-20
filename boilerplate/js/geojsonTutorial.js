258d225245ac43a40e6547b89c74c83678179f4e
Yanbing Chen <yanbing.chen@mail.mcgill.ca>
2024-02-20 17:12:47 -0600

    geojson tutorial updated

--- boilerplate/js/geojsonTutorial.js
+++ boilerplate/js/geojsonTutorial.js
@@ -1,6 +1,6 @@
 /* Example from Leaflet GeoJSON Tutorial */
 
-var map = L.map('map').setView([39.75, -104.99], 13);
+var map = L.map('map').setView([39.75, -104.99], 3);
 
 //add tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,7 +22,7 @@ var geojsonFeature = {
 };
 
 // Create GeoJSON and add it to the map
-L.geoJSON(geojsonFeature).addTo(map);
+//L.geoJSON(geojsonFeature).addTo(map);
 
 //  GeoJSON is passed as an array of valid GeoJSON objects
 var myLines = [{
@@ -39,6 +39,59 @@ var myStyle = {
     "opacity": 0.65
 };
 
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+
+var myLayer = L.geoJSON().addTo(map);
+// myLayer.addData(geojsonFeature);
+//myLayer.addData(myLines);
+
 L.geoJSON(myLines, {
     style: myStyle
+}).addTo(map);
+
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
 }).addTo(map);
